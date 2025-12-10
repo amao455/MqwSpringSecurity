46e6a7e04175314cdfa8106658661f417c6f2ca0
mqw <1315406616@qq.com>
2025-12-11 00:07:41 +0800

    12-11：搭建准备环境

--- SpringSecurity_1/src/main/java/com/zmkg/springsecurity_1/SpringSecurity1Application.java
+++ SpringSecurity_1/src/main/java/com/zmkg/springsecurity_1/SpringSecurity1Application.java
@@ -2,12 +2,14 @@ package com.zmkg.springsecurity_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringSecurity1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecurity1Application.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(SpringSecurity1Application.class, args);
+        System.out.println("111");
     }
 
 }
